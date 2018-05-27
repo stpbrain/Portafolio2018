0c1aab51c2ba8e4dc22eade932b969a0df3773c1
Carlos Poblete Araya <carlos.poblete.araya@gmail.com>
2018-05-27 00:58:42 -0400

    Trabajo 26-05-2018

---
+++ Documentacion/2 Etapa/Sql Carlos/Datos/Datos_Tabla_RUBRO.sql
@@ -0,0 +1,39 @@
+INSERT INTO RUBRO (DSC_RUBRO) VALUES ('Servicios de Comida');
+
+INSERT INTO RUBRO (DSC_RUBRO) VALUES ('Alimentación');
+
+INSERT INTO RUBRO (DSC_RUBRO) VALUES ('Bebidas');
+
+INSERT INTO RUBRO (DSC_RUBRO) VALUES ('Cafeterias y Servicios');
+
+INSERT INTO RUBRO (DSC_RUBRO) VALUES ('Abastecedoras de Carne');
+
+INSERT INTO RUBRO (DSC_RUBRO) VALUES ('Alimentos Congelados');
+
+INSERT INTO RUBRO (DSC_RUBRO) VALUES ('Alimentos Naturales');
+
+INSERT INTO RUBRO (DSC_RUBRO) VALUES ('Elaboración de Fiambres');
+
+INSERT INTO RUBRO (DSC_RUBRO) VALUES ('Frutas y Verduras');
+
+INSERT INTO RUBRO (DSC_RUBRO) VALUES ('Panificadoras');
+
+INSERT INTO RUBRO (DSC_RUBRO) VALUES ('Pescados y Mariscos');
+
+INSERT INTO RUBRO (DSC_RUBRO) VALUES ('Productos Lácteos');
+
+INSERT INTO RUBRO (DSC_RUBRO) VALUES ('Repostería');
+
+INSERT INTO RUBRO (DSC_RUBRO) VALUES ('Venta por Mayor de Alimentos');
+
+INSERT INTO RUBRO (DSC_RUBRO) VALUES ('Golosinas por Mayor');
+
+INSERT INTO RUBRO (DSC_RUBRO) VALUES ('Fiambres y Quesos');
+
+INSERT INTO RUBRO (DSC_RUBRO) VALUES ('Elaboración de Alimentos');
+
+INSERT INTO RUBRO (DSC_RUBRO) VALUES ('Carnicerías');
+
+INSERT INTO RUBRO (DSC_RUBRO) VALUES ('Abastecedoras de Carnes');
+
+INSERT INTO RUBRO (DSC_RUBRO) VALUES ('Helados');
